9d89c52f734b510ebe18d817d46fc5296a94b331
João Breder <joao.g.breder@gmail.com>
2021-11-02 13:12:22 -0300

    Blank Project

---
+++ app/android/app/src/main/java/com/joaobreder/qrcodereader/MainActivity.java
@@ -0,0 +1,5 @@
+package com.joaobreder.qrcodereader;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ app/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.joaobreder.qrcodereader',
+  appName: 'Ionic Capacitor QR Code Reader',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
